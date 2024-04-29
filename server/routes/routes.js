60d44d7f9614a309e51331467280cb5f4ef1f17c
dci-student <hugon1992@gmail.com>
2024-04-29 13:26:55 +0200

    routes.js edit: CRUD Ops for Job created

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -116,4 +116,44 @@ router.delete("/news/:id", authenticateToken, async (req, res) => {
   }
 });
 
+// Job CRUD operations
+router.post("/jobs", authenticateToken, async (req, res) => {
+  const newJob = new Job({ ...req.body, postedBy: req.user.userId });
+  try {
+    await newJob.save();
+    res.status(201).send(newJob);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/jobs", async (req, res) => {
+  try {
+    const jobs = await Job.find().populate("postedBy");
+    res.status(200).send(jobs);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.put("/jobs/:id", authenticateToken, async (req, res) => {
+  try {
+    const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).send(updatedJob);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/jobs/:id", authenticateToken, async (req, res) => {
+  try {
+    await Job.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 module.exports = router;
