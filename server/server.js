d3750289435aaf482b3f240f317be28ccce9828a
dci-student <hugon1992@gmail.com>
2024-04-29 13:50:04 +0200

    server.js edit: configuration for Content Security Policy CSP added

--- server/server.js
+++ server/server.js
@@ -2,9 +2,48 @@ const express = require("express");
 const connectDB = require("./dbconnect.js");
 const { PORT } = require("./dotenvconfig.js");
 const appRoutes = require("./routes/routes.js");
-
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const session = require("express-session");
+const rateLimit = require("express-rate-limit");
 const app = express();
+
 app.use(express.json());
+
+// Konfiguration der Content Security Policy CSP
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'", "https:"],
+      imgSrc: ["'self'", "data:", "https:"],
+      connectSrc: ["'self'", "wss:", "https:"],
+      upgradeInsecureRequests: [],
+    },
+  })
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 3600000, // 1 Stunde
+    },
+  })
+);
 
 connectDB();
 
