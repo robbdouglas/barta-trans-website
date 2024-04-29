ed0e845e99db3a3b94da2b9aa6eac3a7a67319f7
dci-student <hugon1992@gmail.com>
2024-04-29 13:25:51 +0200

    routes.js edit: CRUD Ops for News created

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -76,4 +76,44 @@ router.delete("/users/:id", authenticateToken, async (req, res) => {
   }
 });
 
+// News CRUD operations
+router.post("/news", authenticateToken, async (req, res) => {
+  const newNews = new News({ ...req.body, author: req.user.userId });
+  try {
+    await newNews.save();
+    res.status(201).send(newNews);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/news", async (req, res) => {
+  try {
+    const news = await News.find().populate("author");
+    res.status(200).send(news);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.put("/news/:id", authenticateToken, async (req, res) => {
+  try {
+    const updatedNews = await News.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).send(updatedNews);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/news/:id", authenticateToken, async (req, res) => {
+  try {
+    await News.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 module.exports = router;
