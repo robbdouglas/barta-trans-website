bf9665f75c88cc9a7a52b56d2964c4dc544bbfe6
robbdouglas <robert.nimmrich@icloud.com>
2024-04-30 09:36:01 +0200

    create components directory and add basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Welcome from "./components/Welcome";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      {/* <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="*" element={<h1>ERROR 404: PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router> */}
+    </>
   );
-}
+};
 
 export default App;
