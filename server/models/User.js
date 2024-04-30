1aa86662f62969f2882c8e2ad84370619a9b7f31
dci-student <hugon1992@gmail.com>
2024-04-30 09:39:06 +0200

    refactoring. subscrubeUserSchema

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  isAdmin: { type: Boolean, required: true, default: false }, // Standardwert ist false
+  // email: { type: String, required: true, unique: true },
+  isAdmin: { type: Boolean, required: true, default: true },
+  isSuperUser: { type: Boolean, required: true, default: false }, // Standardwert ist false
 });
 
 module.exports = mongoose.model("User", userSchema);
