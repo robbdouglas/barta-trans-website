6411e56128bd6a1f84e4da452f64dafa419cd22a
dci-student <hugon1992@gmail.com>
2024-04-30 10:31:55 +0200

    Model:new path -role

--- server/models/User.js
+++ server/models/User.js
@@ -4,8 +4,14 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   // email: { type: String, required: true, unique: true },
-  isAdmin: { type: Boolean, required: true, default: true },
-  isSuperUser: { type: Boolean, required: true, default: false }, // Standardwert ist false
+  // isAdmin: { type: Boolean, required: true, default: true },
+  // isSuperUser: { type: Boolean, required: true, default: false }, // Standardwert ist false
+  role: {
+    type: String,
+    required: true,
+    default: "admin",
+    enum: ["admin", "superuser"],
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
