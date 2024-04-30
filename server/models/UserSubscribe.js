fa92bcded497afb4411d248207777a5ae4142be0
dci-student <hugon1992@gmail.com>
2024-04-30 15:48:56 +0200

    delete firstName and lastName key

--- server/models/UserSubscribe.js
+++ server/models/UserSubscribe.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const newsletterSubscriberSchema = new mongoose.Schema({
+const userSubscribeSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  firstName: { type: String },
-  lastName: { type: String },
+  // firstName: { type: String },
+  // lastName: { type: String },
   subscribedAt: { type: Date, default: Date.now }, // Das Datum, an dem der Benutzer sich angemeldet hat
   isSubscribed: { type: Boolean, default: true }, // Gibt an, ob der Benutzer für den Newsletter angemeldet ist (standardmäßig true)
 });
