09fde20cfe3095f5ceff9fe757fc739aaffc0424
dci-student <hugon1992@gmail.com>
2024-04-30 15:46:08 +0200

    add new comment

--- server/nodemailer/email.js
+++ server/nodemailer/email.js
@@ -1,6 +1,6 @@
 //email.js
 const nodemailer = require("nodemailer");
-const dotenv = require("dotenv"); 
+const dotenv = require("dotenv");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -10,22 +10,24 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-router.post('/send-newsletter', async (req, res) => {
+// if new job or news is created, send email to all subscribed users
+router.post("/send-newsletter", async (req, res) => {
   const subscribers = await UserSubscribe.find({ isSubscribed: true });
   const mailOptions = {
     from: process.env.EMAIL_USERNAME,
-    subject: 'Ihr Newsletter',
-    text: 'Hier könnte Ihr Newsletter-Text stehen...',
+    subject: "Ihr Newsletter",
+    text: "Hier könnte Ihr Newsletter-Text stehen...",
   };
-  const sendMailPromises = subscribers.map(subscriber =>
+  const sendMailPromises = subscribers.map((subscriber) =>
     transporter.sendMail({ ...mailOptions, to: subscriber.email })
   );
   try {
     await Promise.all(sendMailPromises);
-    res.status(200).send('Newsletter wurde an alle abonnierten Nutzer gesendet.');
+    res
+      .status(200)
+      .send("Newsletter wurde an alle abonnierten Nutzer gesendet.");
   } catch (error) {
-    console.log('Fehler beim Senden des Newsletters:', error);
-    res.status(500).send('Fehler beim Senden des Newsletters');
+    console.log("Fehler beim Senden des Newsletters:", error);
+    res.status(500).send("Fehler beim Senden des Newsletters");
   }
 });
-
