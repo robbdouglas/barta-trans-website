68b9923577d2c7b2aff1875ce8b9ca3c58a453ef
dci-student <oliver.sporl@dci-student.org>
2024-05-13 09:49:24 +0200

    add cors

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 
 app.use(express.json());
+app.use(cors()); 
+
 
 // Configuration of Content Security Policy CSP
 app.use(
