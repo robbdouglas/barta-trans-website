a187af2199e30c076a9900954e30e8fefbb82288
robbdouglas <robert.nimmrich@icloud.com>
2024-05-14 09:14:14 +0200

    edit index.css: change background color and color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,18 @@
+import "../styles/Header.css";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="header-container">
+        <div className="header-logo">
+          <img src="../images/logo-complete.png" alt="logo" />
+        </div>
+        <div className="burger-menu">
+          <i className="fa-solid fa-bars"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
+import Header from "./Header";
 
 function Welcome() {
   return (
     <div>
+      <Header />
       <h1>Welcome</h1>
       <p>This is the Welcome page.</p>
       <Link to="/news">
