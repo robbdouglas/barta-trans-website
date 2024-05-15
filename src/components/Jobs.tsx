ac605d12727e1f0d29196b8bbddacdb1d24ac694
robbdouglas <robert.nimmrich@icloud.com>
2024-05-15 15:57:14 +0200

    add jobs and news components and css

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -1,5 +1,49 @@
+import "../styles/Jobs.css";
+import Footer from "./Footer";
+import Header from "./Header";
+
 function Jobs() {
-  return <div>Jobs</div>;
+  return (
+    <div>
+      <Header />
+      <section className="jobs-container">
+        <h1>Jobs</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          earum neque totam consequuntur accusantium corporis eaque? Nam eaque
+          assumenda, ex quis reiciendis dolorum dignissimos qui ipsam libero
+          quas veritatis enim ducimus, ipsa soluta. Delectus quaerat minima
+          doloribus inventore corporis, distinctio nulla architecto perferendis
+          nisi asperiores quas, saepe quam ut laudantium.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          earum neque totam consequuntur accusantium corporis eaque? Nam eaque
+          assumenda, ex quis reiciendis dolorum dignissimos qui ipsam libero
+          quas veritatis enim ducimus, ipsa soluta. Delectus quaerat minima
+          doloribus inventore corporis, distinctio nulla architecto perferendis
+          nisi asperiores quas, saepe quam ut laudantium.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          earum neque totam consequuntur accusantium corporis eaque? Nam eaque
+          assumenda, ex quis reiciendis dolorum dignissimos qui ipsam libero
+          quas veritatis enim ducimus, ipsa soluta. Delectus quaerat minima
+          doloribus inventore corporis, distinctio nulla architecto perferendis
+          nisi asperiores quas, saepe quam ut laudantium.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          earum neque totam consequuntur accusantium corporis eaque? Nam eaque
+          assumenda, ex quis reiciendis dolorum dignissimos qui ipsam libero
+          quas veritatis enim ducimus, ipsa soluta. Delectus quaerat minima
+          doloribus inventore corporis, distinctio nulla architecto perferendis
+          nisi asperiores quas, saepe quam ut laudantium.
+        </p>
+      </section>
+      <Footer />
+    </div>
+  );
 }
 
 export default Jobs;
