7c40c9fcb248a5e58d6acff96c7e7c83b26c5097
robbdouglas <robert.nimmrich@icloud.com>
2024-05-16 09:07:56 +0200

    add Services and Contact component and style

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,49 @@
+import "../styles/Contact.css";
+import Footer from "./Footer";
+import Header from "./Header";
+
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <Header />
+      <section className="contact-container">
+        <h1>Contact</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          earum neque totam consequuntur accusantium corporis eaque? Nam eaque
+          assumenda, ex quis reiciendis dolorum dignissimos qui ipsam libero
+          quas veritatis enim ducimus, ipsa soluta. Delectus quaerat minima
+          doloribus inventore corporis, distinctio nulla architecto perferendis
+          nisi asperiores quas, saepe quam ut laudantium.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          earum neque totam consequuntur accusantium corporis eaque? Nam eaque
+          assumenda, ex quis reiciendis dolorum dignissimos qui ipsam libero
+          quas veritatis enim ducimus, ipsa soluta. Delectus quaerat minima
+          doloribus inventore corporis, distinctio nulla architecto perferendis
+          nisi asperiores quas, saepe quam ut laudantium.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          earum neque totam consequuntur accusantium corporis eaque? Nam eaque
+          assumenda, ex quis reiciendis dolorum dignissimos qui ipsam libero
+          quas veritatis enim ducimus, ipsa soluta. Delectus quaerat minima
+          doloribus inventore corporis, distinctio nulla architecto perferendis
+          nisi asperiores quas, saepe quam ut laudantium.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          earum neque totam consequuntur accusantium corporis eaque? Nam eaque
+          assumenda, ex quis reiciendis dolorum dignissimos qui ipsam libero
+          quas veritatis enim ducimus, ipsa soluta. Delectus quaerat minima
+          doloribus inventore corporis, distinctio nulla architecto perferendis
+          nisi asperiores quas, saepe quam ut laudantium.
+        </p>
+      </section>
+      <Footer />
+    </div>
+  );
 }
 
 export default Contact;
