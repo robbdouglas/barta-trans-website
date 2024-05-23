b80c37aef8b262fd6f668ea19aa116714e305e2e
dci-student <oliver.sporl@dci-student.org>
2024-05-23 18:36:41 +0200

    Header component edited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import  { useState, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import '../styles/Header.css';
 import { Link } from 'react-router-dom';
