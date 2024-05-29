a36f1e105d5e68083317e81d5588e226a7589f67
robbdouglas <robert.nimmrich@icloud.com>
2024-05-29 09:48:06 +0200

    fix Services tsx and css: text now starts after header

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import '../styles/Services.css';
-import Footer from './Footer';
-import Header from './Header';
+import { useTranslation } from "react-i18next";
+import "../styles/Services.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function Services() {
   const { t } = useTranslation();
@@ -11,22 +10,22 @@ function Services() {
     <div>
       <Header />
       <section className="services-container">
-        <h1>{t('services.title')}</h1>
-        <h2>{t('services.domesticTransport.title')}</h2>
-        <p>{t('services.domesticTransport.description')}</p>
-        <h2>{t('services.internationalTransport.title')}</h2>
-        <p>{t('services.internationalTransport.description')}</p>
-        <h2>{t('services.seaAirFreight.title')}</h2>
-        <p>{t('services.seaAirFreight.description')}</p>
-        <h2>{t('services.futurePlans.title')}</h2>
-        <p>{t('services.futurePlans.description')}</p>
-        <h2>{t('services.vehicleFleet.title')}</h2>
-        <p>{t('services.vehicleFleet.description')}</p>
-        <h3>{t('services.ftlFleet.title')}</h3>
-        <h4>{t('services.ftlFleet.walkingFloor.title')}</h4>
-        <p>{t('services.ftlFleet.walkingFloor.description')}</p>
-        <h4>{t('services.ftlFleet.curtainSided.title')}</h4>
-        <p>{t('services.ftlFleet.curtainSided.description')}</p>
+        <h1>{t("services.title")}</h1>
+        <h2>{t("services.domesticTransport.title")}</h2>
+        <p>{t("services.domesticTransport.description")}</p>
+        <h2>{t("services.internationalTransport.title")}</h2>
+        <p>{t("services.internationalTransport.description")}</p>
+        <h2>{t("services.seaAirFreight.title")}</h2>
+        <p>{t("services.seaAirFreight.description")}</p>
+        <h2>{t("services.futurePlans.title")}</h2>
+        <p>{t("services.futurePlans.description")}</p>
+        <h2>{t("services.vehicleFleet.title")}</h2>
+        <p>{t("services.vehicleFleet.description")}</p>
+        <h3>{t("services.ftlFleet.title")}</h3>
+        <h4>{t("services.ftlFleet.walkingFloor.title")}</h4>
+        <p>{t("services.ftlFleet.walkingFloor.description")}</p>
+        <h4>{t("services.ftlFleet.curtainSided.title")}</h4>
+        <p>{t("services.ftlFleet.curtainSided.description")}</p>
       </section>
       <Footer />
     </div>
