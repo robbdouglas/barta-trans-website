82657e101ee573e3dc4f56b91c03cd0989cb90c1
dci-student <hugon1992@gmail.com>
2024-06-03 12:15:49 +0200

    Fix bug's with Hamburger Menu on Mobile View

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
-import "../styles/Header.css";
 import { Link } from "react-router-dom";
+import "../styles/Header.css";
 import logo from "../assets/logo-complete.png";
 
 function Header() {
@@ -35,37 +35,56 @@ function Header() {
   }, []);
 
   return (
-    <div>
-      <div className={`header-container ${isMenuOpen ? "menu-open" : ""}`}>
-        <div className="header-logo">
-          <Link to={"/"}>
-            <img src={logo} alt="logo" />
-          </Link>
+    <div className={`header-container ${isMenuOpen ? "menu-open" : ""}`}>
+      <div className="header-logo">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">{t("navbar.home")}</Link>
+          </li>
+          <li>
+            <Link to="/news">{t("navbar.news")}</Link>
+          </li>
+          <li>
+            <Link to="/services">{t("navbar.services")}</Link>
+          </li>
+          <li>
+            <Link to="/jobs">{t("navbar.jobs")}</Link>
+          </li>
+          <li>
+            <Link to="/contact">{t("navbar.contact")}</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">{t("navbar.dashboard")}</Link>
+          </li>
+          <li>
+            <Link to="/login">{t("navbar.login")}</Link>
+          </li>
+        </ul>
+        <div className="languages-container">
+          <button className="language-btn" onClick={() => changeLanguage("de")}>
+            DE
+          </button>
+          <button className="language-btn" onClick={() => changeLanguage("en")}>
+            EN
+          </button>
+          <button className="language-btn" onClick={() => changeLanguage("sk")}>
+            SLO
+          </button>
         </div>
-        <nav className="navbar">
-          <ul>
-            <li>
-              <Link to="/">{t("navbar.home")}</Link>
-            </li>
-            <li>
-              <Link to="/news">{t("navbar.news")}</Link>
-            </li>
-            <li>
-              <Link to="/services">{t("navbar.services")}</Link>
-            </li>
-            <li>
-              <Link to="/jobs">{t("navbar.jobs")}</Link>
-            </li>
-            <li>
-              <Link to="/contact">{t("navbar.contact")}</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">{t("navbar.dashboard")}</Link>
-            </li>
-            <li>
-              <Link to="/login">{t("navbar.login")}</Link>
-            </li>
-          </ul>
+      </nav>
+      <div className="burger-menu" onClick={toggleMenu}>
+        <i className="fa-solid fa-bars"></i>
+      </div>
+      <div className="menu" ref={menuRef}>
+        <div className="menu-header">
+          <button className="close-menu-btn" onClick={closeMenu}>
+            X
+          </button>
           <div className="languages-container">
             <button
               className="language-btn"
@@ -86,64 +105,33 @@ function Header() {
               SLO
             </button>
           </div>
-        </nav>
-
-        <div className="burger-menu" onClick={toggleMenu}>
-          <i className="fa-solid fa-bars"></i>
         </div>
-        <div className="menu" ref={menuRef}>
-          <div className="menu-header">
-            <button className="close-menu-btn" onClick={closeMenu}>
-              X
-            </button>
-            <div className="languages-container">
-              <button
-                className="language-btn"
-                onClick={() => changeLanguage("de")}
-              >
-                DE
-              </button>
-              <button
-                className="language-btn"
-                onClick={() => changeLanguage("en")}
-              >
-                EN
-              </button>
-              <button
-                className="language-btn"
-                onClick={() => changeLanguage("sk")}
-              >
-                SLO
-              </button>
+        <ul>
+          <li>
+            <Link to="/">{t("navbar.home")}</Link>
+          </li>
+          <li>
+            <Link to="/news">{t("navbar.news")}</Link>
+          </li>
+          <li>
+            <Link to="/services">{t("navbar.services")}</Link>
+          </li>
+          <li>
+            <Link to="/jobs">{t("navbar.jobs")}</Link>
+          </li>
+          <li>
+            <Link to="/contact">{t("navbar.contact")}</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">{t("navbar.dashboard")}</Link>
+          </li>
+          <li>_______________</li>
+          <li>
+            <div className="admin-login-container">
+              <Link to="/login">{t("navbar.login")}</Link>
             </div>
-          </div>
-          <ul>
-            <li>
-              <Link to="/">{t("navbar.home")}</Link>
-            </li>
-            <li>
-              <Link to="/news">{t("navbar.news")}</Link>
-            </li>
-            <li>
-              <Link to="/services">{t("navbar.services")}</Link>
-            </li>
-            <li>
-              <Link to="/jobs">{t("navbar.jobs")}</Link>
-            </li>
-            <li>
-              <Link to="/contact">{t("navbar.contact")}</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">{t("navbar.dashboard")}</Link>
-            </li>
-            <li>_______________</li>
-            <li>
-              <div className="admin-login-container">
-                <Link to="/login">{t("navbar.login")}</Link>
-              </div>
-            </li>
-          </ul>
-        </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
