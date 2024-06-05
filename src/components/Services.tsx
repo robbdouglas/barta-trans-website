f8b3fcaba92ff5178a28f17aba59a6bb71cc0e44
dci-student <hugon1992@gmail.com>
2024-06-05 11:49:25 +0200

    Edit Services.tsx and .css for styling banner of Services Page

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import "../styles/Services.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import services_banner from "../assets/imgs/DALL·E 2024-05-22 12.47.42 - A minimalist illustration of international transport. Show a simplified map of Europe with interconnected routes extending beyond to symbolize a globa.webp";
 
 function Services() {
   const { t } = useTranslation();
@@ -10,6 +11,9 @@ function Services() {
     <div>
       <Header />
       <section className="services-container">
+        <div className="services-banner-container">
+          <img className="services-banner" src={services_banner} alt="" />
+        </div>
         <h1>{t("services.title")}</h1>
         <h2>{t("services.domesticTransport.title")}</h2>
         <p>{t("services.domesticTransport.description")}</p>
