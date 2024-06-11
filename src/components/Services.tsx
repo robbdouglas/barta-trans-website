357d7f567256398fda7fc2e1dff52a9bb9f15835
dci-student <hugon1992@gmail.com>
2024-06-11 15:57:45 +0200

    Edit Services.css and Services.tsx- edit style for Job.tsx and News.tsx bugs

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,6 +3,7 @@ import "../styles/Services.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import services_banner from "../assets/imgs/DALL·E 2024-05-22 12.47.42 - A minimalist illustration of international transport. Show a simplified map of Europe with interconnected routes extending beyond to symbolize a globa.webp";
+import "../styles/Services.css";
 
 function Services() {
   const { t } = useTranslation();
@@ -12,24 +13,42 @@ function Services() {
       <Header />
       <section className="services-container">
         <div className="services-banner-container">
-          <img className="services-banner" src={services_banner} alt="" />
+          <img
+            className="services-banner"
+            src={services_banner}
+            alt="One Lkw with Text News"
+          />
         </div>
+
         <h1>{t("services.title")}</h1>
-        <h2>{t("services.domesticTransport.title")}</h2>
-        <p>{t("services.domesticTransport.description")}</p>
-        <h2>{t("services.internationalTransport.title")}</h2>
-        <p>{t("services.internationalTransport.description")}</p>
-        <h2>{t("services.seaAirFreight.title")}</h2>
-        <p>{t("services.seaAirFreight.description")}</p>
-        <h2>{t("services.futurePlans.title")}</h2>
-        <p>{t("services.futurePlans.description")}</p>
-        <h2>{t("services.vehicleFleet.title")}</h2>
-        <p>{t("services.vehicleFleet.description")}</p>
-        <h3>{t("services.ftlFleet.title")}</h3>
-        <h4>{t("services.ftlFleet.walkingFloor.title")}</h4>
-        <p>{t("services.ftlFleet.walkingFloor.description")}</p>
-        <h4>{t("services.ftlFleet.curtainSided.title")}</h4>
-        <p>{t("services.ftlFleet.curtainSided.description")}</p>
+        <div className="services-list-container">
+          <div className="service-list-item">
+            <h2>{t("services.domesticTransport.title")}</h2>
+            <p>{t("services.domesticTransport.description")}</p>
+          </div>
+          <div className="service-list-item">
+            <h2>{t("services.internationalTransport.title")}</h2>
+            <p>{t("services.internationalTransport.description")}</p>
+          </div>
+          <div className="service-list-item">
+            <h2>{t("services.seaAirFreight.title")}</h2>
+            <p>{t("services.seaAirFreight.description")}</p>
+          </div>
+
+          <div className="service-list-item">
+            <h2>{t("services.vehicleFleet.title")}</h2>
+            <p>{t("services.vehicleFleet.description")}</p>
+            <h3>{t("services.ftlFleet.title")}</h3>
+            <h4>{t("services.ftlFleet.walkingFloor.title")}</h4>
+            <p>{t("services.ftlFleet.walkingFloor.description")}</p>
+            <h4>{t("services.ftlFleet.curtainSided.title")}</h4>
+            <p>{t("services.ftlFleet.curtainSided.description")}</p>
+          </div>
+          <div className="service-list-item">
+            <h2>{t("services.futurePlans.title")}</h2>
+            <p>{t("services.futurePlans.description")}</p>
+          </div>
+        </div>
       </section>
       <Footer />
     </div>
