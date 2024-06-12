85030f857f0bfc967ccdb43d5318a2ed96e04eaf
dci-student <hugon1992@gmail.com>
2024-06-12 13:24:47 +0200

    Add Impressum.ccs to /styles folder and add style for Impressum.tsx

--- src/components/Impressum.tsx
+++ src/components/Impressum.tsx
@@ -1,31 +1,43 @@
-import { useTranslation } from 'react-i18next';
-import Footer from './Footer';
-import Header from './Header';
-// import '../styles/Impressum.css';
+import { useTranslation } from "react-i18next";
+import Footer from "./Footer";
+import Header from "./Header";
+import "../styles/Impressum.css";
 
 function Impressum() {
   const { t } = useTranslation();
 
   return (
-    <div className="impressum-container">
+    <div>
       <Header />
-      <h1>{t('impressum.title')}</h1>
-      <p><strong>{t('impressum.info')}</strong></p>
-      <p>{t('impressum.company')}</p>
-      
-      <p><strong>{t('impressum.representedBy')}</strong></p>
-      <p>{t('impressum.representatives')}</p>
-      
-      <p><strong>{t('impressum.contact')}</strong></p>
-      <p>{t('impressum.phone1')}</p>
-      <p>{t('impressum.phone2')}</p>
-      <p>{t('impressum.email')}</p>
-      
-      <p><strong>{t('impressum.vatNumber')}</strong></p>
-      <p>{t('impressum.vat')}</p>
-      
-      <p><strong>{t('impressum.responsible')}</strong></p>
-      <p>{t('impressum.responsiblePerson')}</p>
+      <div className="impressum-container">
+        <h1>{t("impressum.title")}</h1>
+        <p>
+          <strong>{t("impressum.info")}</strong>
+        </p>
+        <p>{t("impressum.company")}</p>
+
+        <p>
+          <strong>{t("impressum.representedBy")}</strong>
+        </p>
+        <p>{t("impressum.representatives")}</p>
+
+        <p>
+          <strong>{t("impressum.contact")}</strong>
+        </p>
+        <p>{t("impressum.phone1")}</p>
+        <p>{t("impressum.phone2")}</p>
+        <p>{t("impressum.email")}</p>
+
+        <p>
+          <strong>{t("impressum.vatNumber")}</strong>
+        </p>
+        <p>{t("impressum.vat")}</p>
+
+        <p>
+          <strong>{t("impressum.responsible")}</strong>
+        </p>
+        <p>{t("impressum.responsiblePerson")}</p>
+      </div>
       <Footer />
     </div>
   );
